6485f1a5b265dc7f7d356979f443afb4219c552e
shearryl000 <67680402+shearryl000@users.noreply.github.com>
2020-12-22 01:14:02 -0700

    delete comment

--- factollection/urls.py
+++ factollection/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('logout/', views.logoutUser, name='logout'),
     path('home/', views.home, name='home'),
     path('index/', views.index, name='index'),
-    # path('index/ss/<int:subject>',views.get_facts, name="index_new"),
     path('details/<int:user_sheet_id>', views.sheet_detail, name='sheet_detail'),
     path('addfact/<str:fact_text>/<int:sub>/<str:fact_type>', views.add_fact, name='add_fact'),
     path('updatenote/<int:user_sheet_id>/<str:note_text>/', views.save_note, name='save_note'),
