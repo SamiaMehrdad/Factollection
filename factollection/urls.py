d03edb0dbd35ac22b9c8535c723be47270d7d6db
shearryl000 <67680402+shearryl000@users.noreply.github.com>
2020-12-22 01:05:35 -0700

    add update note and delete sheet routes

--- factollection/urls.py
+++ factollection/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     # path('index/ss/<int:subject>',views.get_facts, name="index_new"),
     path('details/<int:user_sheet_id>', views.sheet_detail, name='sheet_detail'),
     path('addfact/<str:fact_text>/<int:sub>/<str:fact_type>', views.add_fact, name='add_fact'),
+    path('updatenote/<int:user_sheet_id>/<str:note_text>/', views.save_note, name='save_note'),
+    path('deletesheet/<int:user_sheet_id>', views.delete_sheet, name='delete_sheet'),
 ]
