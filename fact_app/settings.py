9db742675647cc756ff3e28d0a1e87b2f7d28edf
shearryl000 <67680402+shearryl000@users.noreply.github.com>
2020-12-22 16:53:08 -0700

    dotenv to environ

--- fact_app/settings.py
+++ fact_app/settings.py
@@ -78,6 +78,7 @@ WSGI_APPLICATION = 'fact_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
