73ac430c84bd2202e32c326e9b064da6a0fdad31
shearryl000 <67680402+shearryl000@users.noreply.github.com>
2020-12-22 16:08:54 -0700

    heroku env vars

--- fact_app/settings.py
+++ fact_app/settings.py
@@ -11,9 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import django_heroku
 from pathlib import Path
-from dotenv import load_dotenv
 import os
-load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
